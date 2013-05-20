5c8067e1cae56abb2ba071ef0fb7aa812f2b5a60
adrienbaron <adrienbaron.pro@gmail.com>
2013-05-20 20:16:27 +0200

    Changed Canvas.js

--- js/Canvas.js
+++ js/Canvas.js
@@ -6,10 +6,37 @@ var Canvas = Class.extend({
 
 	init: function()
 	{
-		this.element = $("#main")[0];
-		this.context = this.element.getContext("2d");
+		this.element = $("#main");
 
-
+		this.context = this.element[0].getContext("2d");
 		this.context.fillRect(0, 200, 500, 300);
+
+		var that = this;
+		this.element.mousemove(function (event) {
+			that.onClick(event);
+		});
+
+		this.element.mousedown(function (event) {
+			that.okCircle = true;
+		});
+
+		this.element.mouseup(function (event) {
+			that.okCircle = false;
+		});
+
+		this.element.mouseout(function (event) {
+			that.okCircle = false;
+		});
+	},
+
+	onClick: function(event)
+	{
+		if (!this.okCircle) {
+			return;
+		}
+		this.context.beginPath();
+		this.context.arc(event.pageX - 5, event.pageY - 5, 10, 0, Math.PI*2, true); 
+		this.context.closePath();
+		this.context.fill();
 	}
 });
